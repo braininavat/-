c8d9952d82ead8c04d776c9ea274ffde7a58ac3c
braininavat <wjdrkddnr@pusan.ac.kr>
2022-12-11 23:47:37 +0900

    update

--- Spring실습/testproject/src/main/java/com/example/testproject/controller/HelloController.java
+++ Spring실습/testproject/src/main/java/com/example/testproject/controller/HelloController.java
@@ -2,10 +2,13 @@ package com.example.testproject.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -26,5 +29,29 @@ public class HelloController {
         LOGGER.error("error log");
     }
 
+    @PostMapping("/exception")
+    public void exceptionTest() throws Exception{
+        throw new Exception();
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    //ResponsEntity 는 HttpEntity(Http 요청 또는 응답에 해당하는 Httpheader,HttpBody 포함 클래스)
+    //를 상속받아 구현. 결과 데이터 및 HTTP 상태 코드를 제어 가능.
+    //사용자의 Http Request 에 대한 응답 데이터가 포함된다.
+    public ResponseEntity<Map<String,String>> ExceptionHandler(Exception e){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        //responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json";
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.info(e.getLocalizedMessage());
+        LOGGER.info("Controller 내 ExceptionHandler 호출");
+
+        Map<String,String> map = new HashMap<>();
+        map.put("error type",httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message","에러 발생");
+
+        return new ResponseEntity<>(map,responseHeaders,httpStatus);
+    }
 
 }
---
+++ Spring실습/testproject/src/main/java/com/example/testproject/exception/RestExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.example.testproject.exception;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+//컨트롤러 단에서 발생하는 예외처리 여기서.
+//ControllerAdvice 와 ResponseBody(객체 리턴) 를 합친 어노테이션.
+//try-catch 는 지저분할수밖에 없는 코드(Clean Code)- Throw 를 사용해도 되지만 어노테이션으로 예외처리 가능해짐
+//@Service 의 Bean 에서는 동작하지 않는다.
+public class RestExceptionHandler {
+    private final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandler.class);
+
+    @ExceptionHandler(value = Exception.class)
+    //특정 예외 클래스를 지정하면 해당 예외 발생시, 이 메서드가 처리함
+    public ResponseEntity<Map<String,String>> ExceptionHandler(Exception e){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        //responseHeaders.add(HttpHeaders.CONTENT_TYPE, "application/json";
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        LOGGER.info("Advice 내 ExceptionHandler 호출");
+
+        Map<String,String> map = new HashMap<>();
+        map.put("error type",httpStatus.getReasonPhrase());
+        map.put("code", "400");
+        map.put("message","에러 발생");
+
+        return new ResponseEntity<>(map,responseHeaders,httpStatus);
+    }
+
+}
